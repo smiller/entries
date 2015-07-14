41f14fb1726a721def229509783577af4800a999
Sean Miller <sean@seanmiller.ca>
2015-07-14 13:55:32 -0400

    Add mulitple indent levels test

--- spec/entry_builder_spec.rb
+++ spec/entry_builder_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe EntryBuilder, "#build" do
       lines_match(expected, b.build)
     end
   end
-  context "single indent level" do
+  context "multiple indent levels" do
     it "returns Dante entry" do
       raw = ["> E quella a me: “Nessun maggior dolore",
              "che ricordarsi del tempo felice",
@@ -44,6 +44,28 @@ RSpec.describe EntryBuilder, "#build" do
       Line.new(1, "ne la miseria; e ciò sa ’l tuo dottore."),
       Line.new(""),
       Line.new(1, "Ma s’a conoscer la prima radice")])
+        b = EntryBuilder.new(raw)
+      lines_match(expected, b.build)
+    end
+  end
+  context "multiple indent levels" do
+    it "returns Macbeth entry" do
+      raw = ["> LENNOX",
+             "> > Sent he to Macduff?",
+             "",
+             "> LORD",
+             "> > He did: and with an absolute 'Sir, not I,’",
+             "The cloudy messenger turns me his back,",
+             "And hums, as who should say 'You'll rue the time",
+             "That clogs me with this answer.’"]
+      expected = Entry.new([Line.new(1, "LENNOX"),
+      Line.new(2, "Sent he to Macduff?"),
+      Line.new(""),
+      Line.new(1, "LORD"),
+      Line.new(2, "He did: and with an absolute 'Sir, not I,’"),
+      Line.new(2, "The cloudy messenger turns me his back,"),
+      Line.new(2, "And hums, as who should say 'You'll rue the time"),
+      Line.new(2, "That clogs me with this answer.’")])
         b = EntryBuilder.new(raw)
       lines_match(expected, b.build)
     end
