c14630a102d04006a6212ba1b267b4a383bffc0d
Sean Miller <sean@seanmiller.ca>
2015-07-16 11:37:26 -0400

    Entry#paragraphs should trim leading and trailing blank lines

--- lib/entry.rb
+++ lib/entry.rb
@@ -7,12 +7,24 @@ class Entry
 
   def paragraphs
     return [] if @lines.count == 0
-    @lines.inject([[]]) do |result, line|
+    lines_trimmed.inject([[]]) do |result, line|
       build_next_result(result, line)
     end
   end
 
   private
+
+  def lines_trimmed
+    start = first_non_blank_index(@lines)
+    finish = first_non_blank_index(@lines.reverse)
+    @lines[start..-(finish + 1)]
+  end
+
+  def first_non_blank_index(lines)
+    lines.each_with_index do |l, index|
+      return index if l.text != ""
+    end
+  end
 
   def build_next_result(result, line)
     unless second_blank_line(result, line)
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -25,4 +25,50 @@ RSpec.describe Entry, "#paragraphs" do
       expect(entry.paragraphs).to eq([first, middle, last])
     end
   end
+  context "two paras with two blank lines" do
+    it "returns two paras" do
+      first = [Line.new("mind the gap...")]
+      blank = Line.new("")
+      second = [Line.new("... safely stowed")]
+      entry = Entry.new([first, blank, blank, second].flatten)
+      expect(entry.paragraphs).to eq([first, second])
+    end
+  end
+
+  context "blanks at beginning" do
+    context "two" do
+      it "returns one para" do
+        blank = Line.new("")
+        second = [Line.new("... safely stowed")]
+        entry = Entry.new([blank, blank, second].flatten)
+        expect(entry.paragraphs).to eq([second])
+      end
+    end
+    context "one" do
+      it "returns one para" do
+        blank = Line.new("")
+        second = [Line.new("... safely stowed")]
+        entry = Entry.new([blank, second].flatten)
+        expect(entry.paragraphs).to eq([second])
+      end
+    end
+  end
+  context "blanks at end" do
+    context "one" do
+      it "returns one para" do
+        first = [Line.new("mind the gap...")]
+        blank = Line.new("")
+        entry = Entry.new([first, blank].flatten)
+        expect(entry.paragraphs).to eq([first])
+      end
+    end
+    context "two" do
+      it "returns one para" do
+        first = [Line.new("mind the gap...")]
+        blank = Line.new("")
+        entry = Entry.new([first, blank, blank].flatten)
+        expect(entry.paragraphs).to eq([first])
+      end
+    end
+  end
 end
